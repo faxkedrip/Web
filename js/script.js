40d6ed503da1f4c8ea48f8ed03b7515d38a7736b
Shinebayar0122 <sssin2211@gmail.com>
2024-01-17 18:51:11 +0800

    nuur setgegdel

--- js/script.js
+++ js/script.js
@@ -61,3 +61,25 @@ var swiper = new Swiper(".best-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 10,
+  cursorGrab: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
