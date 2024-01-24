de392244f523b63e513d60cfbcec72ddbcd2160c
Shinebayar0122 <sssin2211@gmail.com>
2024-01-25 05:40:49 +0800

    ertyui

--- js/fetchh.js
+++ js/fetchh.js
@@ -21,3 +21,37 @@ function fetchAndDisplayAuthors() {
         .catch(error => console.error('Error fetching data:', error));
 }
 fetchAndDisplayAuthors();
+
+document.addEventListener('DOMContentLoaded', function () {
+    const jsonUrl = 'https://api.jsonbin.io/v3/b/65aeffa3266cfc3fde7e3427/latest';
+    fetch(jsonUrl)
+        .then(response => response.json())
+        .then(data => {
+            const books = data.record;
+            const bookList = document.querySelector('.book-list');
+            books.forEach(book => {
+                const bookElement = document.createElement('article');
+                bookElement.className = 'book';
+                bookElement.innerHTML = `
+                    <img src="${book.pic}" alt="${book.name}">
+                    <h3>${book.name}</h3>
+                    <p>Author: ${book.author}</p>
+                    <p>Price: $${book.price.toFixed(2)}</p>
+                    <a href="#" class="btn">Сагсанд хийх</a>
+                `;
+                bookList.appendChild(bookElement);
+            });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+  });
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    const categoryButton = document.getElementById('categoryButton');
+    const sidebar = document.querySelector('.sidebar');
+    const mainSection = document.querySelector('section');
+  
+    categoryButton.addEventListener('click', function () {
+        sidebar.classList.toggle('active');
+        mainSection.style.marginRight = sidebar.classList.contains('active') ? '20%' : '0';
+    });
+  });
